2b7f2a6dd4578b7785fe1068d982670abcf10899
ItsXyzzy <craig.dowdall56@gmail.com>
2026-08-05 23:12:36 +0100

    Update site and base URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://itsxyzzy.github.io',
+  base: '/pawswick-rescue',
   vite: {
     server: {
       allowedHosts: true
