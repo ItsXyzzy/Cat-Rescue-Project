6f3c88dcdb02bc344d6c6cad1a09a99f674b9240
ItsXyzzy <craig.dowdall56@gmail.com>
2026-08-05 23:14:55 +0100

    Change base path for site configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://itsxyzzy.github.io',
-  base: '/pawswick-rescue',
+  base: '/Cat-Rescue-Project',
   vite: {
     server: {
       allowedHosts: true
